65ba52238b19627d73440ec90fe2fb1023b392c1
Saad Badat <saadbadat@MacBookPro.lan>
2025-11-21 19:16:41 -0500

    changed font

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
 // postcss.config.js (ESM)
 export default {
   plugins: {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: [
+            "Gill Sans",
+            "Gill Sans MT",
+            "Calibri",
+            "Trebuchet MS",
+            "sans-serif",
+          ],
+        },
+      },
+    },
     "@tailwindcss/postcss": {},
   },
 };
